e55b813a6ebfde3c0bae1f26b66e00bc4dba9857
gioohOliveira <go4267994@gmail.com>
2023-05-18 08:28:10 -0300

    aula de orientacao objetos,declarar,instanci,definicao,acionar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       // Aula de orientacao objetos
+        // Declarar objetos
+        pessoa adao;
+        // Instanciar objetos
+
+        adao = new pessoa();
+        pessoa eva= new pessoa();
+
+        // Definição do formato
+
+        adao.nome = "Adão";
+        adao.sobrenome = "Silva";
+        eva.nome= "Eva";
+        eva.sobrenome= "Silva";
+        // Acionar comportamento
+
+        adao.falar();
+        eva.falar();
+
     }
 }
---
+++ src/pessoa.java
@@ -0,0 +1,10 @@
+public class pessoa {
+    // Atributod da classe
+    String nome;
+    String sobrenome;
+    // Método da classe
+    public void falar(){
+        System.out.println("falei");
+    }
+
+}
